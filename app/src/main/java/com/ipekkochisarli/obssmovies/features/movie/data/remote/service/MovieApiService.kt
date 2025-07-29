ff473e743654f869580e769f3a00e341bcacbb0f
Ipek Kochisarli <ipekkochisarli@gmail.com>
2025-07-29 20:20:31 +0300

    fix api key query in MovieApiService

--- app/src/main/java/com/ipekkochisarli/obssmovies/features/movie/data/remote/service/MovieApiService.kt
+++ app/src/main/java/com/ipekkochisarli/obssmovies/features/movie/data/remote/service/MovieApiService.kt
@@ -2,6 +2,7 @@ package com.ipekkochisarli.obssmovies.features.movie.data.remote.service
 
 import com.ipekkochisarli.obssmovies.BuildConfig.API_KEY
 import com.ipekkochisarli.obssmovies.features.movie.data.remote.dto.MovieResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,10 +10,10 @@ interface MovieApiService {
 
     @GET(POPULAR_MOVIES)
     suspend fun getPopularMovies(
-        @Query("apiKey") apiKey: String = API_KEY,
+        @Query("api_key") apiKey: String = API_KEY,
         @Query("language") language: String = LANGUAGE,
         @Query("page") page: Int
-    ): MovieResponse
+    ): Response<MovieResponse>
 
     companion object {
         const val POPULAR_MOVIES = "movie/popular"

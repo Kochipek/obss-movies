a2771b59514d1a468d6415bd333d5f44f3d2eba1
Ipek Kochisarli <ipekkochisarli@gmail.com>
2025-08-05 20:02:25 +0300

    Implement AuthRepository and AuthRepositoryImpl for user authentication

---
+++ app/src/main/java/com/ipekkochisarli/obssmovies/features/login/data/AuthRepositoryImpl.kt
@@ -0,0 +1,25 @@
+package com.ipekkochisarli.obssmovies.features.login.data
+
+import com.google.firebase.auth.FirebaseUser
+import com.ipekkochisarli.obssmovies.core.network.ApiResult
+import com.ipekkochisarli.obssmovies.features.login.data.firebase.AuthDataSource
+import com.ipekkochisarli.obssmovies.features.login.domain.AuthRepository
+import javax.inject.Inject
+
+class AuthRepositoryImpl
+    @Inject
+    constructor(
+        private val authenticationDataSource: AuthDataSource,
+    ) : AuthRepository {
+        override suspend fun registerUser(
+            email: String,
+            password: String,
+        ): ApiResult<FirebaseUser> = authenticationDataSource.registerUser(email, password)
+
+        override suspend fun loginUser(
+            email: String,
+            password: String,
+        ): ApiResult<FirebaseUser> = authenticationDataSource.loginUser(email, password)
+
+        override suspend fun checkIfEmailExists(email: String): ApiResult<Boolean> = authenticationDataSource.checkIfEmailExists(email)
+    }
---
+++ app/src/main/java/com/ipekkochisarli/obssmovies/features/login/domain/AuthRepository.kt
@@ -0,0 +1,18 @@
+package com.ipekkochisarli.obssmovies.features.login.domain
+
+import com.google.firebase.auth.FirebaseUser
+import com.ipekkochisarli.obssmovies.core.network.ApiResult
+
+interface AuthRepository {
+    suspend fun registerUser(
+        email: String,
+        password: String,
+    ): ApiResult<FirebaseUser>
+
+    suspend fun loginUser(
+        email: String,
+        password: String,
+    ): ApiResult<FirebaseUser>
+
+    suspend fun checkIfEmailExists(email: String): ApiResult<Boolean>
+}
